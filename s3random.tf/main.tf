cd97842d30e3e4a7e1a38a6e646eba6f02c5e00c
vaishnavigulhane19 <vaishnavigulhane530@gmail.com>
2026-07-24 23:32:24 +0530

    vdhbjn

--- s3random.tf/main.tf
+++ s3random.tf/main.tf
@@ -7,7 +7,7 @@ resource "random_id" "bucket" {
 
 resource "aws_s3_bucket" "bucket" {
     count = 10
-    bucket = "bucket-of-navi-khsn-${random_id.bucket.hex}"
+    bucket = "bucket-of-navi-khsn-${random_id.bucket[count.index].hex}"
     force_destroy = true
     tags = {
         Name = "bucket-${count.index + 1}"
