55e3d6dc16158ac624ea583ed3eea4a5aeba6f7a
vaishnavigulhane19 <vaishnavigulhane530@gmail.com>
2026-07-24 23:16:10 +0530

    chbdx

--- s3bucket/main.tf
+++ s3bucket/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "bucket" {
     count = 10 
-    bucket = "my-navi-bucket-navi-uhi-${index + 1}"
-    force_destroy = true 
+    bucket = "my-navi-bucket-navi-uhi-${count.index + 1}"
+    force_destroy = true
 }
